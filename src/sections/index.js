6ce6e29b720e1a49474a6f1527d3d76954fdc732
udaykumar93986 <udaykumarsarvepalli22@gmail.com>
2023-08-25 15:54:24 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./customerReviews";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  Footer,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  CustomerReviews,
+};
